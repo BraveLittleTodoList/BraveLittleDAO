258764840b50c2a7bc56485da9162edee8a57441
Carlos Chapa <bravelittletodolist@gmail.com>
2022-04-21 13:26:40 -0600

    Added connect wallet button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { useAddress, useMetamask } from "@thirdweb-dev/react";
+
 const App = () => {
-  return (
+
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+
+  if(!address){
+    return (
+      <div className="landing">
+        <h1>Welcome to Brave Little DAO</h1>
+        <button onClick={connectWithMetamask} className="btn-hero">Connect your wallet</button>
+      </div>
+    );
+}
+  return(
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>Wallet connected!</h1>
     </div>
-  );
-};
+  )
+}
 
 export default App;
